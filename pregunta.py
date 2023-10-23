4c497e94e402da3ca6eb0333255d3007561a3999
carolinag02 <145158322+carolinag02@users.noreply.github.com>
2023-10-23 01:33:57 -0500

    Laboratorio 5
    Entrega Laboratorio 5

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,21 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
-
+    df.drop(['Unnamed: 0'], axis=1, inplace = True)
+    df.dropna(inplace = True)
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+    df["idea_negocio"] = df["idea_negocio"].str.lower()
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).replace("-"," ").replace("_"," "))
+    df["barrio"] = df["barrio"].str.lower()
+    df["barrio"] = df["barrio"].apply(lambda x: str(x).replace("_"," ").replace("-"," "))
+    df["estrato"] = df["estrato"].replace('01', 1).astype(int)
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].fillna(0).astype(int)
+    df["fecha_de_beneficio"] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True, format='mixed')
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).strip("$").strip().replace(".00","").replace(",",""))
+    df["línea_credito"] = df["línea_credito"].str.lower()
+    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).replace("-"," ").replace("_"," "))
+    
+    df.drop_duplicates(inplace = True)
+    
     return df
